bd70eb20b8ba1f31e0803f92a8687fe2a41eaeba
tamdoduc <82079157+tamdoduc@users.noreply.github.com>
2023-01-18 13:57:16 +0700

    Login,SignUp,Profile

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    TextView btOpenRegister;
+    Button btLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(),Profile.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        btOpenRegister = (TextView) findViewById(R.id.btOpenRegister);
+        btOpenRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(),SignUp.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    TextView btOpenLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        btOpenLogin = (TextView) findViewById(R.id.btOpenLogin);
+        btOpenLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Login.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
+
+    }
+}
